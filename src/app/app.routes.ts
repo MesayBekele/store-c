56aed9eaad443cfdaced62f17a6bbcb84ec37ad6
Mesay <mesay@its-MacBook-Pro.local>
2024-12-23 18:25:24 -0500

    store update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import {FeaturesComponent} from './pages/features/features.component';
 
-export const routes: Routes = [];
+// Define routes for each component
+export const routes: Routes = [
+  { path: '', component: HomeComponent },  // Default route
+  { path: 'features', component: FeaturesComponent },  // About route
+  { path: 'home', component: HomeComponent }  // Contact route
+];
+
+
